04075898758b9be2156d4316311e15b74ddd4d7f
Darya <ara1015@tut.by>
2025-08-26 14:39:52 +0300

    added new models ('shop')

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
+from tkinter.constants import CASCADE
+
+from django.utils import timezone
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
